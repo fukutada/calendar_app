c461a3f78837c0960f4f4bb0dbcf2fbd52f4d988
fukutada <92067046+fukutada@users.noreply.github.com>
2022-01-13 12:38:47 +0900

    henkou

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCIye01X2kcoK-_AA3xGz3f_K54-S-CV_8",
+    authDomain: "calendar-d7ed5.firebaseapp.com",
+    projectId: "calendar-d7ed5",
+    storageBucket: "calendar-d7ed5.appspot.com",
+    messagingSenderId: "873532324050",
+    appId: "1:873532324050:web:a11a2126426ad8dd30150b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
